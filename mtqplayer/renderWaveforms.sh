aac688933185c41698d1fdfbdd427a0217ac42f4
adius <mail@adriansieber.com>
2014-01-31 10:08:06 +0100

    Improved waveform script, updated waveforms

--- mtqplayer/renderWaveforms.sh
+++ mtqplayer/renderWaveforms.sh
@@ -3,5 +3,6 @@
 for song in songs/*.mp3
 do
 	echo "Generate waveform for ${song##*/}"
-	waveform $song --width 2000 --color-center 464646ff --color-outer 464646ff ${song%.mp3}.png
+	waveform $song --width 2000 --color-center c8c8c8ff --color-outer c8c8c8ff --scan ${song%.mp3}.png
+	convert ${song%.mp3}.png -rotate 90 ${song%.mp3}.png
 done
