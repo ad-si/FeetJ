d783cdd4d0b2fcafaec19bcb8155315b11d767ec
adius <mail@adriansieber.com>
2014-07-09 10:40:24 +0200

    Removed unnecessary files, added paper, added MIT-license

--- bombgame/source/include/events/Events.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "DebugEvent.h"
-#include "PositionEvent.h"
-#include "ContactEvent.h"
-#include "TapEvent.h"
--- bombgame/source/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "core/Application.h"
-
-int main(int argc, char **argv)
-{
-	mtq::Application app(argc, argv);
-	return app.exec();
-}
